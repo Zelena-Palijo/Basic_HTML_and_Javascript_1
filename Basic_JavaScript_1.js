a8a906f9d60762b5cc756589a8d83ad6cbe42e8d
Zelena-Palijo <98482883+Zelena-Palijo@users.noreply.github.com>
2022-07-25 20:29:58 -0700

    Included function for basic html 2 file

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,1 +1,8 @@
 alert("I am an alert box!");
+
+	function My_First_Function(){
+		var String = "THIS spicy!";
+		var result = String.fontcolor("orange");
+		document.getElementById("Cheeto").innerHTML =
+		result;
+	}
